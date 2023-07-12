122c5480ee92aa5ac66455cba28ef1a7345d39b1
JulissaDantes <julissadcj@gmail.com>
2023-07-12 14:19:07 -0400

    add send eth

--- contracts/LowLevel.sol
+++ contracts/LowLevel.sol
@@ -1,9 +1,11 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.11;
 
-/* This is just a single place to put the most use low level calls in order to not forget them */
+/* This is just a single place to put the most use low level calls in order to not forget them. This is not to be deployed */
+// https://docs.soliditylang.org/en/v0.8.20/yul.html#
 
 contract LowLevel {
+    address[2] owners = [0xC09758D8fe8fF0545b8005CA2f82947E4dfd3A05, 0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266];
 
     function getValue() public view returns(uint256) {
         assembly {
@@ -17,6 +19,19 @@ contract LowLevel {
         assembly {
             sstore(0, newValue)
         }
+    }
+
+    function sendETH(address to, uint256 amount) public {
+        bool success;
+        assembly {
+            for {let i := 0} lt(i,2) { i := add(i, 1)} {
+                let owner := sload(i)
+                if eq(to, owner) {
+                    success := call(gas(), to, amount, 0, 0, 0, 0)
+                }
+            }
+        }
+        require(success, "Failed");
     }
     
 }
