922e7f35b14c8fed1a372491f8e57eb22bc37941
JulissaDantes <julissadcj@gmail.com>
2023-08-16 15:28:21 -0400

    add array examples

--- contracts/LowLevelYul.sol
+++ contracts/LowLevelYul.sol
@@ -8,8 +8,14 @@ contract LowLevelYul {
     uint256 x = 8;
     address[2] owners = [0xC09758D8fe8fF0545b8005CA2f82947E4dfd3A05, 0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266];
     //the next 2 variables are inside 1 memory slot
-    uint128 a;
-    uint128 b;
+    uint128 A;
+    uint128 B;
+    uint8[2] fixedArray = [1, 0];
+    uint256[] bigArray;
+
+    constructor () {
+        bigArray = [10, 11, 13, 18];
+    }
 
     function getXSlot() external pure returns(uint256 p) {
         assembly{
@@ -19,15 +25,44 @@ contract LowLevelYul {
 
     function getASlotOffset() external pure returns(uint256 slot, uint256 offset) {
         assembly{
-             slot := a.slot
-             offset := a.offset
+             slot := A.slot
+             offset := A.offset
         }
     }
 
     function getBSlotOffset() external pure returns(uint256 slot, uint256 offset) {
         assembly{
-             slot := b.slot
-             offset := b.offset
+             slot := B.slot
+             offset := B.offset
+        }
+    }
+    
+    /*
+    * if I call readInOffset(2,0) returns A, and if I call readInOffset(2,16) returns B
+    */
+    function readInOffset(uint256 slot,uint256 offset) external view returns (uint128 v) {
+        assembly {
+            let data := sload(slot)
+            v := shr(offset, data)//TBD works for A and not for B
+        }
+    }
+
+    function getfixedArrayVal(uint256 index) external view returns(uint8 v) {
+        assembly {
+            v := sload(add(fixedArray.slot, index))
+        }
+    }
+
+    function getdynamicArrayValue(uint256 index) external view returns(uint8 v) {
+        uint256 slot;
+        assembly {
+            slot := bigArray.slot
+        }
+
+        bytes32 location = keccak256(abi.encode(slot));
+        
+        assembly {
+            v := sload(add(location, index))
         }
     }
     
