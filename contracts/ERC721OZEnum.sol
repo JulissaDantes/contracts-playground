50186bb83eda54d1fa4537c4edcc002cef920bfa
JulissaDantes <julissadcj@gmail.com>
2023-09-01 13:50:29 -0400

    add enumerable to benchmark

---
+++ contracts/ERC721OZEnum.sol
@@ -0,0 +1,41 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+
+contract ERC721OZEnum is ERC721, ERC721Enumerable {
+    constructor() ERC721("MyToken", "MTK") {}
+
+    function mint(address to, uint256 tokenId) external {
+        _mint(to, tokenId);
+    }
+
+    function burn(uint256 tokenId) external {
+        _burn(tokenId);
+    }
+    
+    // The following functions are overrides required by Solidity.
+
+    function _beforeTokenTransfer(address from, address to, uint256 tokenId, uint256 batchSize)
+        internal
+        override(ERC721, ERC721Enumerable)
+    {
+        super._beforeTokenTransfer(from, to, tokenId, batchSize);
+    }
+
+    function batchMint(address to, uint256[] memory tokenIds) external {
+        for (uint256 i = 0; i < tokenIds.length; i++) {
+            _mint(to, tokenIds[i]);
+        }
+    }
+
+    function supportsInterface(bytes4 interfaceId)
+        public
+        view
+        override(ERC721, ERC721Enumerable)
+        returns (bool)
+    {
+        return super.supportsInterface(interfaceId);
+    }
+}
--- scripts/benchmark-Azuki-OZ.js
+++ scripts/benchmark-Azuki-OZ.js
@@ -4,6 +4,7 @@ async function main() {
     const [signer1, signer2] = await ethers.getSigners();
     const OZImpl = await ethers.getContractFactory('ERC721OZ');
     const AzukiImpl = await ethers.getContractFactory('ERC721Azuki');
+    const ERC721OZEnum = await ethers.getContractFactory('ERC721OZEnum');
     //TODO include enumerable version to benchmark
     const results = [];
 
@@ -14,41 +15,52 @@ async function main() {
     const erc721a = await AzukiImpl.deploy();
     await erc721a.deployed();
 
+    const erc721Enum = await ERC721OZEnum.deploy();
+    await erc721Enum.deployed();
+
     console.log(`\nBenchmarking Gas Usage for ERC-721 Implementations`);
     // mint
     let tx = await erc721.mint(signer1.address, 1); 
     let rc = await tx.wait();    
     let txa = await erc721a.mint(1); 
     let rca = await txa.wait();
+    let txe = await erc721Enum.mint(signer1.address, 1); 
+    let rce = await txe.wait();
 
-    results.push({ functionName: "Mint", oz: rc.gasUsed.toString(), azuki: rca.gasUsed.toString() });
+    results.push({ functionName: "Mint", oz: rc.gasUsed.toString(), azuki: rca.gasUsed.toString(), enumerable: rce.gasUsed.toString()});
 
     // mint batch
     tx = await erc721.batchMint(signer1.address, [2,3,4,5,6,7,8,9,10,11]); 
     rc = await tx.wait();    
     txa = await erc721a.mint(10); 
     rca = await txa.wait();
-    results.push({ functionName: "Batch mint", oz: rc.gasUsed.toString(), azuki: rca.gasUsed.toString() });
+    txe = await erc721Enum.batchMint(signer1.address, [2,3,4,5,6,7,8,9,10,11]); 
+    rce = await txe.wait();
+    results.push({ functionName: "Batch mint", oz: rc.gasUsed.toString(), azuki: rca.gasUsed.toString(), enumerable: rce.gasUsed.toString() });
 
     // transfer
     tx = await erc721.transferFrom(signer1.address, signer2.address, 1); 
     rc = await tx.wait();    
     txa = await erc721a.transferFrom(signer1.address, signer2.address, 1); 
     rca = await txa.wait();
-    results.push({ functionName: "Transfer", oz: rc.gasUsed.toString(), azuki: rca.gasUsed.toString() });
+    txe = await erc721Enum.transferFrom(signer1.address, signer2.address, 1); 
+    rce = await txe.wait();
+    results.push({ functionName: "Transfer", oz: rc.gasUsed.toString(), azuki: rca.gasUsed.toString(), enumerable: rce.gasUsed.toString() });
 
     // burn
     tx = await erc721.burn(1); 
     rc = await tx.wait();    
     txa = await erc721a.burn(1); 
     rca = await txa.wait();
-    results.push({ functionName: "Burn", oz: rca.gasUsed.toString(), azuki: rca.gasUsed.toString() });
+    txe = await erc721Enum.burn(1); 
+    rce = await txe.wait();
+    results.push({ functionName: "Burn", oz: rca.gasUsed.toString(), azuki: rca.gasUsed.toString(), enumerable: rce.gasUsed.toString() });
 
-    console.log("\nFunction Name |   oz(Normal)   |   azuki | Cheaper");
-    console.log("--------------------------------------------------");
+    console.log("\nFunction Name |   oz(Normal)   |   azuki | Enumerable |  Normal vs Azuki | Enumerable vs Azuki");
+    console.log("----------------------------------------------------------------------------------------------");
     for (let i = 0;i < results.length;i++) {
         console.log(
-            `${results[i].functionName.padEnd(14)}|${results[i].oz.toString().padStart(16)}|${results[i].azuki.toString().padStart(9)}| ${((results[i].oz < results[i].azuki)?'OZ':'Azuki').padStart(7)}`
+            `${results[i].functionName.padEnd(14)}|${results[i].oz.toString().padStart(16)}|${results[i].azuki.toString().padStart(9)}|${results[i].enumerable.toString().padStart(12)}|${((results[i].oz < results[i].azuki)?'OZ':'Azuki').padStart(17)}|${((results[i].enumerable < results[i].azuki)?'Enumerable':'Azuki').padStart(17)}`
         );
     }
     console.log('\n');
